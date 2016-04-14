3457170966d17023c99245802df1c4a316d53777
Nymarra <aisling.n.dempsey@gmail.com>
2016-04-13 20:46:05 -0700

    complete

--- melon_raffle.py
+++ melon_raffle.py
@@ -3,3 +3,14 @@
 # Add code starting here
 # Hint: remember to import any functions you need from
     # other files or libraries
+from random import choice
+from customer_info import organize_customer_data
+
+def winner(customers):
+    chosen_one = choice(customers)
+    print "The winner is %s, they can be reached at %s" % (chosen_one.name, chosen_one.email)
+
+
+customers = organize_customer_data('customers.txt')
+
+winner(customers)
